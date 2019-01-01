218aad81c7ee504dcc1a6c412d7947119e758e99
Ryan Lynn <ryan.lynn@protonmail.com>
2019-01-01 13:50:33 -0500

    updated favicon for realz this time

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,13 +9,13 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-manifest',
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: 'Momentum Developer Conference',
+        short_name: 'momentum',
         start_url: '/',
         background_color: '#663399',
         theme_color: '#663399',
         display: 'minimal-ui',
-        icon: 'src/assets/images/website-icon.png', // This path is relative to the root of the site.
+        icon: 'src/assets/images/Chevrons.png', // This path is relative to the root of the site.
       },
     },
     'gatsby-plugin-sass',
