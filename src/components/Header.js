6f9702aa34e0f523790bcf9f24589cb1b5bfd3c6
Brandon Culp <bculp513@gmail.com>
2019-01-15 17:29:51 -0500

    Added logo to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'gatsby'
+import logo from '../../old_site/public/img/momentum-logo.png';
+import '../assets/css/logo.css';
 
 const Header = (props) => (
     <header id="header" className="alt">
-        <Link to="/" className="logo"><strong>Forty</strong> <span>by HTML5 UP</span></Link>
+        <Link to="/" className="logo" >
+          <img src={logo} />
+        </Link>
         <nav>
             <a className="menu-link" onClick={props.onToggleMenu} href="javascript:;">Menu</a>
         </nav>
