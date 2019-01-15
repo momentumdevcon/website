7992ecca352e932c82f987e863df4aba6d5d9471
Brandon Culp <bculp513@gmail.com>
2019-01-15 17:33:20 -0500

    Pulled logo out of old site code

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'gatsby'
-import logo from '../../old_site/public/img/momentum-logo.png';
+import logo from '../assets/images/momentum-logo.png';
 import '../assets/css/logo.css';
 
 const Header = (props) => (
