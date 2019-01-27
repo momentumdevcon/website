55407ece2470d21ce2d46a20e39533bb4ff09d1c
Brandon Culp <bculp513@gmail.com>
2019-01-26 21:18:09 -0500

    Styled individual session page

--- src/templates/session.js
+++ src/templates/session.js
@@ -3,6 +3,7 @@ import Helmet from 'react-helmet'
 import metaContent from '../assets/data/metaContent.js';
 import { Layout } from '../components';
 import { graphql } from 'gatsby';
+import '../assets/css/session.css';
 
 export default ({ data: { sessionsData }, pageContext: { slug } }) => {
   const session = sessionsData.sessions.find(session => session.alternative_id === slug);
@@ -20,13 +21,22 @@ export default ({ data: { sessionsData }, pageContext: { slug } }) => {
       <div id="main" className="alt">
         <section id="one">
           <div className="inner">
-            <header className="major">
+            <header className="major session-title">
               <h1>{title}</h1>
             </header>
-            <div>{speakerName}</div>
-            <div>Level - {level}</div>
-            <div>Tags: {tags}</div>
-            <div>{session.description}</div>
+            <div className="presenter"><span className="info-prefix">Presented by:</span>{speakerName}</div>
+            <div className="description">{session.description}</div>
+            <div className="levelTags">
+              <span><span className="info-prefix">Level: </span>{level}</span>
+              <span>
+                <span className="info-prefix">Tags:</span>
+                  {
+                    tags.map((tag, index) => (
+                      <span>{`${index !== tags.length - 1 ? `${tag}, ` : tag}`}</span>
+                    ))
+                  }
+              </span>
+            </div>
           </div>
         </section>
       </div>
