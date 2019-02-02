51d5e9c4c0bc0dd467091c2c5daa4a2658d21738
Michael Richardson <probackpacker@gmail.com>
2019-02-02 15:53:38 -0500

    Fix OpenGraph meta tags
    OpenGraph requires "property" instead of "name"

--- src/assets/data/metaContent.js
+++ src/assets/data/metaContent.js
@@ -9,10 +9,10 @@ export default [
   { "name": "twitter:description", "content": "Momentum is a Cincinnati developer conference dedicated to providing developers with great content for learning and success. Join us on March 21, 2019 at Sharonville Convention Center." },
   { "name": "twitter:creator", "content": "@momentumdevcon" },
   { "name": 'twitter:image', "content": chevron },
-  { "name": 'og:title', "content": "Momentum Developer Conference" },
-  { "name": 'og:type', "content": "website" },
-  { "name": 'og:url', "content": "http://momentumdevcon.com" },
-  { "name": 'og:image', "content": logoWPadding },
-  { "name": 'og:description', "content": "Momentum is a Cincinnati developer conference dedicated to providing developers with great content for learning and success. Join us on March 21, 2019 at Sharonville Convention Center." },
-  { "name": 'og:site_name', "content": "Momentum Developer Conference" }
+  { "property": 'og:title', "content": "Momentum Developer Conference" },
+  { "property": 'og:type', "content": "website" },
+  { "property": 'og:url', "content": "http://momentumdevcon.com" },
+  { "property": 'og:image', "content": logoWPadding, "name":"image" },
+  { "property": 'og:description', "content": "Momentum is a Cincinnati developer conference dedicated to providing developers with great content for learning and success. Join us on March 21, 2019 at Sharonville Convention Center." },
+  { "property": 'og:site_name', "content": "Momentum Developer Conference" }
 ]
