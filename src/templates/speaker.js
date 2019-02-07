c99c580a5714f79d10cced6218cd6b59a729f77d
Brandon Culp <bculp513@gmail.com>
2019-02-06 20:44:33 -0500

    Added speaker pg link to speaker image, started styling speaker page

--- src/templates/speaker.js
+++ src/templates/speaker.js
@@ -2,29 +2,41 @@ import React from 'react';
 import { graphql, Link } from 'gatsby';
 import { Layout } from '../components/';
 import generateSocialLink from '../utils/generateSocialLink';
+import getSpeakerSlug from '../utils/getSpeakerSlug';
+import '../assets/css/speaker.css';
 
 export default ({ data: { sessionizeData }, pageContext: { slug } }) => {
   const speaker = sessionizeData.speakers.find((speaker) => {
-    const craftedSlug = `${speaker.firstName.split(' ').join('_')}_${speaker.lastName.split(' ').join('_')}`;
-    return craftedSlug === slug;
+    const speakerSlug = getSpeakerSlug(speaker.firstName, speaker.lastName);
+    return speakerSlug === slug;
   });
   const speakerSessions = sessionizeData.sessions.filter(session =>
     speaker.sessions.includes(parseInt(session.alternative_id)));
 
   return (
     <Layout>
-      <div>{speaker.fullName}</div>
-      <img src={speaker.profilePicture} alt={speaker.fullName} />
-      <div>{speaker.tagLine}</div>
-      <div>{speaker.bio}</div>
-      {speaker.links.map(link => (
-        generateSocialLink(link, 'speakerSocial')
-      ))}
-      {speakerSessions.map(session => (
-        <Link key={session.alternative_id} to={`/session/${session.alternative_id}`}>
-          {session.title}
-        </Link>
-      ))}
+      <div id="main" className="alt">
+        <div className="inner horizontalContainer">
+          <div className="verticalContainer">
+            <img src={speaker.profilePicture} alt={speaker.fullName} />
+            <div>{speaker.tagLine}</div>
+            <div className="horizontalContainer">
+              {speaker.links.map(link => (
+                generateSocialLink(link, 'speakerSocial')
+              ))}
+            </div>
+          </div>
+          <div className="verticalContainer">
+            <div>{speaker.fullName}</div>
+            <div>{speaker.bio}</div>
+            {speakerSessions.map(session => (
+              <Link key={session.alternative_id} to={`/session/${session.alternative_id}`}>
+                {session.title}
+              </Link>
+            ))}
+          </div>
+        </div>
+      </div>
     </Layout>
   );
 };
