2997a935741b7a726ca4c702ff8933e07c8d88a9
Michael Richardson <probackpacker@gmail.com>
2019-02-08 23:11:50 -0500

    Improve speaker page social meta tags
    Update meta content generation and add to speaker page

--- src/assets/data/metaContent.js
+++ src/assets/data/metaContent.js
@@ -1,18 +1,42 @@
-import chevron from '../images/Chevrons-White-Background.png';
-import logoWPadding from '../images/logo-with-padding.png';
+import chevron from '../images/Chevrons-White-Background.png'
+import logoWPadding from '../images/logo-with-padding.png'
 
-export default [
-  { "name": "description", "content": "Momentum is a Cincinnati developer conference dedicated to providing developers with great content for learning and success. Join us on March 21, 2019 at Sharonville Convention Center." },
-  { "name": "twitter:card", "content": "summary" },
-  { "name": "twitter:site", "content": "@momentumdevcon" },
-  { "name": "twitter:title", "content": "Momentum Developer Conference" },
-  { "name": "twitter:description", "content": "Momentum is a Cincinnati developer conference dedicated to providing developers with great content for learning and success. Join us on March 21, 2019 at Sharonville Convention Center." },
-  { "name": "twitter:creator", "content": "@momentumdevcon" },
-  { "name": 'twitter:image', "content": chevron },
-  { "property": 'og:title', "content": "Momentum Developer Conference" },
-  { "property": 'og:type', "content": "website" },
-  { "property": 'og:url', "content": "http://momentumdevcon.com" },
-  { "property": 'og:image', "content": logoWPadding, "name":"image" },
-  { "property": 'og:description', "content": "Momentum is a Cincinnati developer conference dedicated to providing developers with great content for learning and success. Join us on March 21, 2019 at Sharonville Convention Center." },
-  { "property": 'og:site_name', "content": "Momentum Developer Conference" }
+export const mainTitle = 'Momentum Developer Conference'
+export const mainDescription =
+  'Momentum is a Cincinnati developer conference dedicated to providing developers with great content for learning and success. Join us on March 21, 2019 at Sharonville Convention Center.'
+
+const commonMetaContent = [
+  { name: 'description', property: 'og:description', content: mainDescription },
+  { name: 'twitter:card', content: 'summary' },
+  { name: 'twitter:site', content: '@momentumdevcon' },
+  { name: 'twitter:description', content: mainDescription },
+  { name: 'twitter:creator', content: '@momentumdevcon' },
+  { name: 'twitter:image', content: chevron },
+  { property: 'og:type', content: 'website' },
+  // { "property": 'og:url', "content": "http://momentumdevcon.com" },
+  { property: 'og:image', content: logoWPadding, name: 'image' },
+  { property: 'og:site_name', content: 'Momentum Developer Conference' },
 ]
+
+const homeMetaContent = [
+  { name: 'twitter:title', content: mainTitle },
+  { property: 'og:title', content: mainTitle },
+]
+
+export function createMetaContent(title, description, image) {
+  return [
+    { name: 'description', property: 'og:description', content: description },
+    { name: 'twitter:card', content: 'summary_large_image' },
+    { name: 'twitter:title', content: title },
+    { name: 'twitter:site', content: '@momentumdevcon' },
+    { name: 'twitter:description', content: description },
+    { name: 'twitter:creator', content: '@momentumdevcon' },
+    { name: 'twitter:image', content: image },
+    { property: 'og:type', content: 'website' },
+    { property: 'og:image', content: image, name: 'image' },
+    { property: 'og:site_name', content: 'Momentum Developer Conference' },
+    { property: 'og:title', content: title },
+  ]
+}
+
+export default [...commonMetaContent, ...homeMetaContent]
--- src/templates/speaker.js
+++ src/templates/speaker.js
@@ -4,7 +4,7 @@ import { graphql, Link } from 'gatsby';
 import { Layout, Icon } from '../components/';
 import generateSocialLink from '../utils/generateSocialLink';
 import getSpeakerSlug from '../utils/getSpeakerSlug';
-import metaContent from '../assets/data/metaContent';
+import metaContent, { createMetaContent } from '../assets/data/metaContent';
 import '../assets/css/speaker.css';
 
 export default ({ data: { sessionizeData }, pageContext: { slug } }) => {
@@ -16,11 +16,12 @@ export default ({ data: { sessionizeData }, pageContext: { slug } }) => {
     speaker.sessions.includes(parseInt(session.alternative_id)));
   const sessionText = `Session${speakerSessions.length > 1 ? 's' : ''}:`
 
+  const pageTitle = `${speaker.fullName} - Momentum Dev Con`;
   return (
     <Layout>
       <Helmet
-       title={`${speaker.fullName} - Momentum Dev Con`}
-       meta={[...metaContent, {name: "twitter:image", content:speaker.profilePicture}]}
+       title={pageTitle}
+       meta={createMetaContent(pageTitle, `Momentum Speaker Profile - ${speaker.fullName} - ${speaker.tagLine}`, speaker.profilePicture)}
       />
       <div id="main" className="alt">
         <div className="backArrow">
