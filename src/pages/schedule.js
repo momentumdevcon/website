f4094833500c7f6df860c835cd8b4731a9ec53b0
Brandon Culp <bculp513@gmail.com>
2019-02-19 19:16:48 -0500

    Styled schedule and added it to the menu

--- src/pages/schedule.js
+++ src/pages/schedule.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Helmet from 'react-helmet';
 import { BannerLanding, Layout } from '../components';
 import metaContent from '../assets/data/metaContent.js';
+import '../assets/css/schedule.css';
 
 const SchedulePage = () => (
   <Layout>
@@ -13,13 +14,9 @@ const SchedulePage = () => (
     <BannerLanding pageName="Schedule" />
 
     <div id="main" className="alt">
-
-
-     <section id="one">
-        <div className="inner">
-        <iframe style={{width:"100%", height:"5000px"}} scrolling="yes"  src="data:text/html,<script type='text/javascript' src='https://sessionize.com/api/v2/tl3o9lkp/view/gridtable'></script>" />
-        </div>
-      </section>
+      <div className="inner">
+        <iframe className="schedule" src="data:text/html,<script type='text/javascript' src='https://sessionize.com/api/v2/tl3o9lkp/view/gridtable'></script>" />
+      </div>
     </div>
   </Layout>
 );
