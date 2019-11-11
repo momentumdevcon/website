c29987b5ff00a5468dbb026c7c5330e4f1bdeb20
Michael Richardson <probackpacker@gmail.com>
2019-11-10 23:26:56 -0500

    Correct session count

--- src/components/WhatIsMomentum.js
+++ src/components/WhatIsMomentum.js
@@ -13,7 +13,7 @@ const WhatIsMomentum = () => (
       </header>
       <ul>
         <li>
-          35 sessions of outstanding content presented by speakers from
+          30+ sessions of outstanding content presented by speakers from
           Cincinnati, the region, and around the globe
         </li>
         <li>Breakfast, lunch, and happy hour</li>
