15c8a210cee5e38373969d9872f2f0174b52e2d9
Kat Fairbanks <ktornwall@gmail.com>
2020-04-28 08:18:58 -0400

    Move blogs, rename folder
    I setup a sample netlify CMS, and I was incorrect about it having to be in static. Only the config lives there.

--- gatsby-config.js
+++ gatsby-config.js
@@ -74,7 +74,7 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        path: `${__dirname}/static/blogs`,
+        path: `${__dirname}/src/assets/blog`,
         name: `blog`,
       },
     },
