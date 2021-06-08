d6afa2dd962f5a01a0bc91a2147898541eb4eabd
Brandon Culp <bculp513@gmail.com>
2021-06-08 09:41:39 -0400

    Updated CFP text, removed image

--- src/components/CallForPresenters.js
+++ src/components/CallForPresenters.js
@@ -1,25 +1,13 @@
 import React from 'react';
-import CFPBanner from '../assets/images/CFP-Banner.png'
-import '../assets/css/callForPresenters.css'
 
 const CallForPresenters = () => (
   <section id="one" className="tiles whatIsMomentumTiles">
     <article>
       <header className="major cfp">
-        <h3>CFP Now Open!</h3>
-        <a href="https://sessionize.com/momentum-2021" target="_blank" className="no-underline" rel="noopener">
-          <img
-            className="banner"
-            src={CFPBanner}
-            alt="Cfp now open through May 31"
-          />
-        </a>
+        <h3>CFP Now Closed</h3>
       </header>
       <p>
-      We are moving forward with our plans to have an in-person conference on October 15, and have opened up our call for presenters! 
-      If you would like to be considered for presenting at Momentum 2021, put your best ideas together and get them over to us on <a href="https://sessionize.com/momentum-2021">Sessionize</a> 
-       . Wow us with your expertise, or show us something we haven’t already seen. Our speaker selection committee is excited to watch the talks come rolling in. 
-      Our CFP is open May 1-31st, but don’t procrastinate. <a href="https://sessionize.com/momentum-2021">Submit your proposal today!</a>
+        Our 2021 Call for Presenters is now closed. Thank you to everyone who took the time and effort to submit their abstracts! We can't wait to start the review and selection process.
       </p>
     </article>
   </section>
