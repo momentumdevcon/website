280d3f5138b8e1c7aefcf4004147ee68645280e7
Brandon Culp <bculp513@gmail.com>
2022-04-23 11:08:16 -0400

    Made the CFP copy less actionable until the CFP is open

--- src/components/CallForPresenters.js
+++ src/components/CallForPresenters.js
@@ -4,12 +4,12 @@ const CallForPresenters = () => (
   <section id="one" className="tiles whatIsMomentumTiles">
     <article>
       <header className="major">
-        <h3>CFP Open Soon!</h3>
+        <h3>Call for Speakers opens May 1st!</h3>
       </header>
       <p>
-      If you would like to be considered for presenting at Momentum 2022, put your best ideas together and get them over to us on <a href="https://sessionize.com/momentum-2022">Sessionize</a> 
-       . Wow us with your expertise, or show us something we haven’t already seen. Our speaker selection committee is excited to watch the talks come rolling in. 
-      Our CFP is open May 1-31st, but don’t procrastinate. <a href="https://sessionize.com/momentum-2022">Submit your proposal today!</a>
+      If you would like to be considered for presenting at Momentum 2022, 
+      in preparation for the Call for Speakers, put your best ideas together and check out the details on <a href="https://sessionize.com/momentum-2022">Sessionize</a>. 
+      Wow us with your expertise, or show us something we haven’t seen. Our CFP is open May 1-31st!
       </p>
     </article>
   </section>
