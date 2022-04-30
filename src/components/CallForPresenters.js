3034e9ced67ac6f2ebe23da41e350b766d8cfc29
Brandon Culp <bculp513@gmail.com>
2022-04-30 17:15:44 -0400

    CFP copy updated since cfp is now open

--- src/components/CallForPresenters.js
+++ src/components/CallForPresenters.js
@@ -4,12 +4,12 @@ const CallForPresenters = () => (
   <section id="one" className="tiles whatIsMomentumTiles">
     <article>
       <header className="major">
-        <h3>Call for Speakers opens May 1st!</h3>
+        <h3>Call for Speakers Now Open!</h3>
       </header>
       <p>
       If you would like to be considered for presenting at Momentum 2022, 
-      in preparation for the Call for Speakers, put your best ideas together and check out the details on <a href="https://sessionize.com/momentum-2022">Sessionize</a>. 
-      Wow us with your expertise, or show us something we haven’t seen. Our CFP is open May 1-31st!
+      put your best ideas together and get them over to us on <a href="https://sessionize.com/momentum-2022">Sessionize</a>. 
+      Wow us with your expertise, or show us something we haven’t seen. Our CFP is open until May 31st!
       </p>
     </article>
   </section>
