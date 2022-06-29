721e2a6cd6156469e3360d0b7a2e7bffae44d59c
Brandon Culp <bculp513@gmail.com>
2022-06-28 20:48:36 -0400

    Removed some organizers, removed link to blog

--- src/assets/images/organizers/index.js
+++ src/assets/images/organizers/index.js
@@ -1,20 +1,14 @@
 import michael from './michael.jpg';
 import taryn from './taryn.jpg';
 import tracy from './tracy.jpg';
-import izzi from './izzi.jpg';
 import brandon from './brandon.jpg';
 import ryan from './ryan.jpg';
-import jennifer from './jennifer.jpg';
 import jason from './jason.jpg';
 import kat from './kat.jpeg';
-import chris from './chris.jpg';
 
 export default {
   brandon,
-  chris,
-  izzi,
   jason,
-  jennifer,
   kat,
   michael,
   ryan,
