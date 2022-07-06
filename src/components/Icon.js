956eb078a8d63b345d1323f0e8c2180abb654ca4
Brandon Culp <bculp513@gmail.com>
2022-07-06 11:24:20 -0400

    Open external links in a new tab

--- src/components/Icon.js
+++ src/components/Icon.js
@@ -10,7 +10,7 @@ const upperCaseFirstLetter = word => word.replace(word.split('')[0], word.charAt
 
 const Icon = ({ className, icon, label, link, size }) =>
   (link ? (
-    <a href={link} className={classnames('icon alt', { [className]: className })}>
+    <a href={link} className={classnames('icon alt', { [className]: className })} target="_blank" rel="noopener noreferrer">
       <FontAwesomeIcon icon={icon} size={size} />
       <span className="label">{upperCaseFirstLetter(label)}</span>
     </a>
