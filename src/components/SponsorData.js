ec0a6a518038cfe13abc4e7da635af2ab6ceefe2
Ryan Lynn <ryan.lynn@protonmail.com>
2023-05-07 22:01:58 +0000

    add 2023 sponsors

--- src/components/SponsorData.js
+++ src/components/SponsorData.js
@@ -4,160 +4,46 @@ import { graphql } from 'gatsby'
 export const sponsorData = {
   Headlining: [
     {
-      company: 'Check Point',
-      fileName: 'checkpoint.jpg',
-      link: 'https://www.checkpoint.com'
-    },
-    {
       company: 'Fifth Third Bank',
       fileName: 'fifthThird.png',
       link: 'https://www.53.com/content/fifth-third/en.html',
     }
   ],
   Room: [
-    {
-      company: 'Cincinnati State',
-      fileName: 'cincinnatiState.png',
-      link: 'https://cincinnatistate.edu/',
+    { 
+      company: 'Tech Elevator',
+      fileName: 'tech-elevator.png',
+      link: 'https://www.techelevator.com/'
     },
     {
-      company: 'Progress',
-      fileName: 'progress.png',
-      link: 'https://www.progress.com/',
-    },
-    {
-      company: 'RedHat',
-      fileName: 'redHat.png',
-      link: 'https://www.redhat.com/',
-    },
-    {
-      company: 'Launch Scout',
-      fileName: 'launchScout.png',
-      link: 'https://www.launchscout.com/',
-    },
-    {
-      company: 'Seven Hills Technology',
-      fileName: 'sevenHills.png',
-      link: 'https://sevenhillstechnology.com/'
+      company: 'Kroger',
+      link: 'https://www.kroger.com/',
+      fileName: 'kroger.png',
     }
   ],
   'Lunch/Break': [
-    {
-      company: 'alexa',
-      fileName: 'alexa.png',
-      link: 'https://developer.amazon.com/en-US/alexa'
-    }
-  ],
-  Game: [],
-  Booth: [
-    { 
-      company: 'Brooksource',
-      fileName: 'brooksource.png',
-      link: 'https://www.brooksource.com/'
-    },
-    {
-      company: 'Cloverleaf',
-      fileName: 'cloverleaf.png',
-      link: 'https://www.cloverleaf.me'
-    },
-    {
-      company: 'KentuckianaWorks',
-      fileName: 'kentuckianaWorks.png',
-      link: 'https://kentuckianaworks.org',
-    },
-    {
-      company: 'Ingage Partners',
-      fileName: 'ingage.png',
-      link: 'https://www.ingagepartners.com/',
-    },
-    {
-      company: 'Insight',
-      fileName: 'insight.png',
-      link: 'https://www.insight.com'
-    },
-    {
-      company: 'MAX Technical Training',
-      fileName: 'max.png',
-      link: 'https://maxtrain.com/'
-    },
-    {
-      company: 'Ascendum',
-      fileName: 'ascendum.jpg',
-      link: 'https://www.ascendum.com/',
-    },
-    {
-      company: 'ATC',
-      fileName: 'atc.png',
-      link: 'https://www.4atc.com/',
-    },
-    {
-      company: 'Vernovis',
-      fileName: 'vernovis.png',
-      link: 'https://www.vernovis.com/',
-    },
-    {
-      company: 'FusionAuth',
-      fileName: 'fusionauth.png',
-      link: 'https://fusionauth.io/',
-    },
     {
       company: 'Eliassen Group',
       fileName: 'eliassen.png',
       link: 'https://www.eliassen.com/'
     }
   ],
+  Game: [],
+  Booth: [
+    {
+      company: 'Ascendum',
+      fileName: 'ascendum.jpg',
+      link: 'https://www.ascendum.com/',
+    },
+    {
+      company: 'MAX Technical Training',
+      fileName: 'max.png',
+      link: 'https://maxtrain.com/'
+    }
+  ],
   'Speaker Social': [],
-  Lanyard: [
-    { 
-      company: 'Tech Elevator',
-      fileName: 'tech-elevator.png',
-      link: 'https://www.techelevator.com/'
-    },
-  ],
-  'Community Partners': [
-    {
-      company: 'Cincinnati Software Craftsmanship',
-      link: 'https://craftsmanship.dev/'
-    },
-    {
-      company: 'CincyML',
-      link: 'https://www.cincinnatiml.com/'
-    },
-    {
-      company: 'CinciJS',
-    },
-    {
-      company: 'CinJUG',
-    },
-    {
-      company: 'CincyRB',
-      link: 'https://cincyrb.com/'
-    },
-    {
-      company: 'getWITit',
-      link: 'https://getwitit.org/'
-    },
-    {
-      company: 'KCDC',
-      link: 'https://www.kcdc.info/'
-    },
-    {
-      company: 'Ohio Elixir',
-      link: 'https://www.ohioelixir.com/'
-    },
-    {
-      company: 'Stir Trek',
-      link: 'https://stirtrek.com/'
-    },
-    {
-      company: 'Virtual Coffee',
-      link: 'https://virtualcoffee.io/'
-    },
-    {
-      company: 'Women Who Code',
-      link: 'https://www.womenwhocode.com/cincinnati'
-    },
-  ],
+  Lanyard: [],
+  'Community Partners': [],
 }
 
 export const sponsorImage = graphql`fragment sponsorImage on File {
