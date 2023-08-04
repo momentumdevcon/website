8306c04d2824a955eb7446070cc1b71a6222ed55
Michael Richardson <probackpacker@gmail.com>
2023-08-04 04:35:38 +0000

    Update file TicketsCTA.js

--- src/components/TicketsCTA.js
+++ src/components/TicketsCTA.js
@@ -5,7 +5,7 @@ export const TicketsCTA = () => (
   <section id="one" className="tiles whatIsMomentumTiles">
     <article>
       <header className="major">
-        <h3>2023 Early Bird Tickets On Sale Now!</h3>
+        <h3>Early Bird Ticket Sales Close August 15!</h3>
       </header>
       <TitoWidget source="cta-widget" />
     </article>
