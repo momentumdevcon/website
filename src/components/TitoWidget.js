5d699539455800b706a21f7d048a2a7fe55680d1
Michael Richardson <probackpacker@gmail.com>
2023-08-14 16:14:41 +0000

    Add explanatory copy to tickets widget

--- src/components/TitoWidget.js
+++ src/components/TitoWidget.js
@@ -6,6 +6,10 @@ export const TitoWidget = ({ source }) => (
     <Helmet>
       <script src='https://js.tito.io/v2' />
     </Helmet>
+    <h3>Payment Options</h3>
+    <p>Tickets can be paid for through PayPal, which will allow you to enter a credit card, or by invoice.</p>
     <tito-widget event="the-circuit/momentum-2023" source={ source } />
+    <h3>Momentum is a 21-and-up event!</h3>
+    <p>We would love to welcome younger developers to our conference but the Hard Rock Casino does not admit anyone under 21 and participants must pass through the casino.</p>
   </>
 );
