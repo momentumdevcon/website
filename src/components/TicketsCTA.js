6ba210bd06691dee2b7de1ae5bff556f4cd9eead
Michael Richardson <michael.richardson@kroger.com>
2023-08-26 00:47:54 -0400

    Update message for regular price

--- src/components/TicketsCTA.js
+++ src/components/TicketsCTA.js
@@ -5,7 +5,7 @@ export const TicketsCTA = () => (
   <section id="one" className="tiles whatIsMomentumTiles">
     <article>
       <header className="major">
-        <h3>Early Bird Ticket Sales Close August 15!</h3>
+        <h3>Regular Price Tickets Available Through September 14th</h3>
       </header>
       <TitoWidget source="cta-widget" />
     </article>
