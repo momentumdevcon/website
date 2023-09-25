1b8916476ee1f0db8548d885f3d6fe3eff8fd91b
Brandon Culp <bculp513@gmail.com>
2023-09-25 10:15:30 -0600

    Updated ticket CTA for late registration

--- src/components/TicketsCTA.js
+++ src/components/TicketsCTA.js
@@ -5,7 +5,7 @@ export const TicketsCTA = () => (
   <section id="one" className="tiles whatIsMomentumTiles">
     <article>
       <header className="major">
-        <h3>Regular Price Tickets Available Through September 15th</h3>
+        <h3>Late Registration Tickets Available Through October 6th</h3>
       </header>
       <TitoWidget source="cta-widget" />
     </article>
