fa4628e205cecd2f86f0a413c18c4e4b521ef3ac
Dev Agrawal <agrawadv@mail.uc.edu>
2024-05-02 04:52:09 -0400

    cta to join slack

--- src/components/CallForPresenters.js
+++ src/components/CallForPresenters.js
@@ -11,14 +11,27 @@ export const CallForPresenters = () => (
         your best ideas together and get them over to us on Sessionize . Wow us
         with your expertise, or show us something we haven’t seen. Our CFP is
         open until May 31st!
+        <br />
+        <a
+          href="https://sessionize.com/momentum-2024/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Link to CFP
+        </a>
       </p>
-      <a
-        href="https://sessionize.com/momentum-2024/"
-        target="_blank"
-        rel="noreferrer"
-      >
-        Link to CFP
-      </a>
+      <p>
+        Need help with your abstract?{' '}
+        <a
+          href="https://momentumdevcon.slack.com/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Join our Slack group
+        </a>{' '}
+        to talk about your ideas and get help from organizers and other
+        speakers.
+      </p>
     </article>
   </section>
 )
