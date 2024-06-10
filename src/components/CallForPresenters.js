87dd6d6b2f79130ae15fd645ddf3852abb478768
Dev Agrawal <agrawadv@mail.uc.edu>
2024-06-10 16:57:16 -0400

    CFP closed

--- src/components/CallForPresenters.js
+++ src/components/CallForPresenters.js
@@ -4,33 +4,11 @@ export const CallForPresenters = () => (
   <section id="one" className="tiles whatIsMomentumTiles">
     <article>
       <header className="major">
-        <h3>Call for Speakers Now Open!</h3>
+        <h3>Call for Speakers Now Closed!</h3>
       </header>
       <p>
-        If you would like to be considered for presenting at Momentum 2024, put
-        your best ideas together and get them over to us on Sessionize . Wow us
-        with your expertise, or show us something we haven’t seen. Our CFP is
-        open until May 31st!
-        <br />
-        <a
-          href="https://sessionize.com/momentum-2024/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Link to CFP
-        </a>
-      </p>
-      <p>
-        Need help with your abstract?{' '}
-        <a
-          href="https://momentumdevcon.slack.com/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Join our Slack group
-        </a>{' '}
-        to talk about your ideas and get help from organizers and other
-        speakers.
+        Our 2023 Call for Speakers is now closed. 
+        Thank you to everyone who took the time and effort to submit their abstracts!
       </p>
     </article>
   </section>
