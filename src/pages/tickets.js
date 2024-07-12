54757e3069f6377eec75b6d3681cfb05524f556a
bhuynhdev <bhuynhdev@gmail.com>
2024-07-11 22:41:13 -0400

    Update Tito tickets for 2024

--- src/pages/tickets.js
+++ src/pages/tickets.js
@@ -2,17 +2,17 @@ import React from 'react'
 import { TitoWidget, Wrapper } from '../components'
 
 const TicketsPage = () => (
-  <Wrapper title="2023 Tickets">
+  <Wrapper title="2024 Tickets">
     <div id="main" className="alt">
       <section id="one">
         <div className="inner">
-          <h3>Procrastinator Tickets Still Available!</h3>
+          <h3>Early Bird Tickets Now Available!</h3>
           <TitoWidget source="tickets-page-widget" />
           <p>
             *If you encounter any issues with the integrated purchase
             experience, use our <nsbp />
             <a
-              href="https://ti.to/the-circuit/momentum-2023?source=widget-issue"
+              href="https://ti.to/the-circuit/momentum-2024?source=widget-issue"
               target="_blank"
               rel="noopener noreferrer"
             >
