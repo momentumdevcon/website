5fd6a6201fd897fe0a3a486d66e4231345da5741
Bao Huynh <bhuynhdev@gmail.com>
2024-08-16 12:06:16 -0400

    End EarlyBird ticket, change to Regular ticket

--- src/components/TicketsCTA.js
+++ src/components/TicketsCTA.js
@@ -5,7 +5,7 @@ export const TicketsCTA = () => (
   <section id="one" className="tiles whatIsMomentumTiles">
     <article>
       <header className="major">
-        <h3>Early Bird Tickets Available Now!</h3>
+        <h3>Tickets Available Now!</h3>
       </header>
       <TitoWidget source="cta-widget" />
     </article>
