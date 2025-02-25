66bb3680ab8f40832c0697f2b0444faf97e93d40
Dev Agrawal <devagrawal09@gmail.com>
2025-02-25 11:19:43 -0600

    hid momentum app

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -18,13 +18,13 @@ export const Modal = ({ children, formState, updateFormState }) => {
         >
           <button className="banner-btn">Provide Feedback</button>
         </Link>
-        <Link
+        {/* <Link
           to="https://app.momentumdevcon.com/"
           style={{ textDecoration: 'none', borderBottom: 'none', paddingRight: '15px', paddingBottom: '10px' }}
           target="_blank"
         >
           <button className="banner-btn">Momentum App</button>
-        </Link>
+        </Link> */}
         {/*
       <Link to="/blog/what-to-expect-2025" style={{ textDecoration: 'none', borderBottom: 'none', paddingRight: '15px', paddingBottom: '10px'  }}>
       <button  className='banner-btn'>What To Expect</button>
