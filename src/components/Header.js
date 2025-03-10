a39369f98c377f6637ca6efa4aa99cfb1096ed6c
Dev Agrawal <devagrawal09@gmail.com>
2025-03-10 10:25:39 -0500

    circuit logo in navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,17 @@
-import React, { useState } from 'react';
-import { Link } from 'gatsby';
-import { Menu } from './Menu';
-import logo from '../assets/images/cropped-white.svg';
-import '../assets/css/logo.css';
-import '../assets/css/header.css';
+import React, { useState } from 'react'
+import { Link } from 'gatsby'
+import { Menu } from './Menu'
+import logo from '../assets/images/cropped-white.svg'
+import '../assets/css/logo.css'
+import '../assets/css/header.css'
+import circuitLogo from '../assets/circuit-logo.png'
 
 export const Header = () => {
   const [menuState, updateMenuState] = useState(' Closed')
-  
+
   const toggleMenu = () => {
     if (menuState === ' Closed') {
-      return updateMenuState(' Open');
+      return updateMenuState(' Open')
     }
     return updateMenuState(' Closed')
   }
@@ -27,6 +28,13 @@ export const Header = () => {
         <span></span>
       </div>
       <Menu menuState={menuState} />
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
+          <a href="https://www.thecircuit.net/" target="_blank" style={{ border: 'none', height: '45px' }}>
+            <img src={circuitLogo} alt="logo" className="logo icon" style={{ height: '45px' }} />
+          </a>
+        </div>
+      </div>
     </header>
-  );
+  )
 }
