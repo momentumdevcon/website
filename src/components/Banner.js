1af60066351f8680f3751df6d9d8efbeec88b6a7
Matthew Weil <matthewweil3@gmail.com>
2025-07-16 22:30:18 -0400

    adding in the whova link

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -17,6 +17,13 @@ export const Banner = () => (
         </p>
       </div>
       <p className="date">October 16, 2025</p>
+      <div id="whova-regform-widget" data-reactroot=""></div>
+      <div id="whova-regform-seo-widget" data-reactroot="">
+        Powered By <a class="brandlink" target="_blank" href="https://whova.com"><b>Whova</b></a>
+        <br/>
+        <a class="brandanchorlink" target="_blank" href="https://whova.com/event-registration-software">Conference registration system</a>
+        </div>
+        <script src="https://whova.com/static/frontend/xems/js/whova-regform-widget.js?eid=HYDIeQD2e6sV8sR@9Ok4&amp;host=https://whova.com&amp;registration=attendee&amp;regpagetoken=" type="text/javascript" id="whova-embeded-regform-script" data-reactroot=""></script>
       <div style={{ display: 'flex', gap: '4px' }}>
         <InterestForm />
       </div>
