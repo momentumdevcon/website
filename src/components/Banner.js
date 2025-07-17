65388a1c5327f6b897db65a6f2cd63eebcdca54b
Matthew Weil <matthewweil3@gmail.com>
2025-07-16 22:41:37 -0400

    messed up format

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -17,7 +17,27 @@ export const Banner = () => (
         </p>
       </div>
       <p className="date">October 16, 2025</p>
-      <a style="text-decoration:none;border-radius:3px;padding:10px;width:200px;font-family:Helvetica;line-height:1.6;font-weight:400;font-size:16px;display:block;text-align:center;background-color:#2dacee;color:#FFFFFF" target="_blank" rel="noreferrer" href="https://whova.com/portal/registration/HYDIeQD2e6sV8sR@9Ok4/" data-reactroot="">Register Now on Whova</a>    
+      <a
+  style={{
+    textDecoration: 'none',
+    borderRadius: '3px',
+    padding: '10px',
+    width: '200px',
+    fontFamily: 'Helvetica',
+    lineHeight: 1.6,
+    fontWeight: 400,
+    fontSize: '16px',
+    display: 'block',
+    textAlign: 'center',
+    backgroundColor: '#2dacee',
+    color: '#FFFFFF',
+  }}
+  target="_blank"
+  rel="noreferrer"
+  href="https://whova.com/portal/registration/HYDIeQD2e6sV8sR@9Ok4/"
+>
+  Register Now on Whova
+</a>
       <div style={{ display: 'flex', gap: '4px' }}>
         <InterestForm />
       </div>
