0b95fcf231d50dc131f2773da178a704c115fe07
bhuynhdev <baoh.inbox@gmail.com>
2025-07-23 00:25:47 -0400

    Hide 2024 schedule

--- src/pages/schedule.js
+++ src/pages/schedule.js
@@ -6,7 +6,8 @@ const SchedulePage = () => (
   <Wrapper title="2025 Schedule">
     <div id="main" className="alt">
       <div className="inner">
-        <ScheduleTable />
+        {/* <ScheduleTable /> */}
+        Stay tuned for our 2025 schedule.
       </div>
     </div>
   </Wrapper>
