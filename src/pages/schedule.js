ad9afd55b0eb1c6597d42bf7706b977a299b9179
Michael Richardson <probackpacker@gmail.com>
2025-10-06 23:13:52 -0400

    Fix Lint Errors
    Refactor LightningTalk component: add noreferrer to external link and clean up formatting; update ScheduleTable and TicketsCTA components for consistency; adjust SchedulePage layout for better indentation

--- src/pages/schedule.js
+++ src/pages/schedule.js
@@ -6,7 +6,7 @@ const SchedulePage = () => (
   <Wrapper title="2025 Schedule">
     <div id="main" className="alt">
       <div className="inner">
-      <ScheduleTable />
+        <ScheduleTable />
       </div>
     </div>
   </Wrapper>
