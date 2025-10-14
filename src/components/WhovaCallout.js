289f8a476b0e56825168f9e4af50f399d348fe19
Matthew Weil <matthewweil3@gmail.com>
2025-10-13 22:03:12 -0400

    Whova add in

---
+++ src/components/WhovaCallout.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const WhovaCallout = () => (
+  <section id="one" className="tiles whatIsMomentumTiles">
+    <article>
+      <header className="major">
+        <h3>Download the Whova App!</h3>
+      </header>
+      <p>We’re excited to share that Whova will be your hub for this year’s conference experience. Download the app to explore the full agenda, curate your own session lineup, and take part in lively discussions before the event begins. Don’t forget to check out the icebreakers and speaker questions already waiting for you!</p>
+    </article>
+  </section>
+)
