b7a1e95325fa5d72911830815b4efb274cf1a317
Kat Fairbanks <ktornwall@gmail.com>
2026-04-30 20:31:24 -0400

    Add CFP for 2026
    - Update year, end date (moved to June 1st so it lands on Monday)
    - Switch from Slack to Discord
    - Update formatting, CTA button style

--- src/components/CallForPresenters.js
+++ src/components/CallForPresenters.js
@@ -7,7 +7,7 @@ export const CallForPresentersClosed = () => (
         <h3>Call for Speakers Now Closed!</h3>
       </header>
       <p>
-        Our 2025 Call for Speakers is now closed. Thank you to everyone who took the time and effort to submit their
+        Our 2026 Call for Speakers is now closed. Thank you to everyone who took the time and effort to submit their
         abstracts!
       </p>
     </article>
@@ -21,18 +21,24 @@ export const CallForPresentersOpen = () => (
         <h3>Call for Speakers Now Open!</h3>
       </header>
       <p>
-        If you would like to be considered for presenting at Momentum 2025, put your best ideas together and get them
-        over to us on Sessionize . Wow us with your expertise, or show us something we haven't seen. Our CFP is open
-        until May 31st!
-        <br />
-        <a href="https://sessionize.com/momentum-2025/" target="_blank" rel="noreferrer">
-          Link to CFP
+        If you would like to be considered for presenting at Momentum 2026, put your best ideas together and get them
+        over to us on Sessionize. Wow us with your expertise, or show us something we haven't seen. Our CFP is open
+        until <strong>June 1st!</strong>
+      </p>
+      <p>
+        <a
+          href="https://sessionize.com/momentum-2026/"
+          target="_blank"
+          rel="noreferrer"
+          style={{ marginBottom: '1rem', textDecoration: 'none', borderBottom: 'none' }}
+        >
+          <button className='banner-btn'>Submit a Session</button>
         </a>
       </p>
       <p>
         Need help with your abstract?{' '}
-        <a href="https://momentumdevcon.slack.com/" target="_blank" rel="noreferrer">
-          Join our Slack group
+        <a href="https://discord.momentumdevcon.com/" target="_blank" rel="noreferrer">
+          Join our Discord
         </a>{' '}
         to talk about your ideas and get help from organizers and other speakers.
       </p>
