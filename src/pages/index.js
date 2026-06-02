b5ee8fa1d299cf665886c52840b79895328eaee7
Matthew Weil <matthewweil3@gmail.com>
2026-06-01 22:00:36 -0400

    removed CFP callout

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,9 +10,9 @@ const HomeIndex = ({ data }) => (
     <Helmet title="Momentum Developer Conference" meta={[...metaContent]} />
     <Banner />
     <div id="main">
-      <CallForPresentersOpen />
-      {/*<WhovaCallout />
-    <TicketsCTA />*/}
+      {/*<CallForPresentersOpen />
+      /*<WhovaCallout />
+      TicketsCTA />*/}
       {/* <ThankYou /> */}
       <WhatIsMomentum />
       <LatestBlogPost posts={data.allMarkdownRemark.edges} />
