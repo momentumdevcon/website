571387a0b5ea46e1f75e70d40e8cfbee16f1616f
Michael Richardson <michaelrichardson@Michaels-MacBook-Pro.local>
2026-07-20 07:27:35 -0400

    Set Momentum 2026 Whova registration URL

--- src/assets/data/ticketing.js
+++ src/assets/data/ticketing.js
@@ -1,5 +1,7 @@
 // Gatsby replaces GATSBY_* variables at build time for browser bundles.
-// eslint-disable-next-line no-undef
-export const whovaRegistrationUrl = process.env.GATSBY_WHOVA_REGISTRATION_URL || ''
+export const whovaRegistrationUrl =
+  // eslint-disable-next-line no-undef
+  process.env.GATSBY_WHOVA_REGISTRATION_URL ||
+  'https://whova.com/portal/registration/2ev2ReOea-5UJ4yBtC2-/'
 
 export const ticketsAreAvailable = Boolean(whovaRegistrationUrl)
